333ac2041a9a5289220505786e79f344f81fa527
jamesjnadeau <james.nadeau@gmail.com>
2014-09-30 12:16:52 -0400

    adding initial files

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+echo '<h3>this is a test of the google app engine getDefaultGoogleStorageBucketName</h3>';
+
+$bucket = 'will_this_work';
+
+echo '<p>The bucket is '.$bucket.'</p>';
+
+$file = "gs://".$bucket."/test.txt";
+
+file_put_contents($file, 'This is a test, please work' );
+sleep(2);
+echo file_get_contents($file);
+
+
+echo '<h3>PHP code used here</h3>';
+
+echo '<pre style="padding: 10px; background-color: lightgrey"">
+'.htmlentities(file_get_contents( $_SERVER['DOCUMENT_ROOT'].$_SERVER['PHP_SELF'] )).'
+</pre>';
